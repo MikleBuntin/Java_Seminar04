b035fcc0c71f22724400ec4f083b779bf1650f66
MikleBuntin <BuntinMA@mail.ru>
2023-02-23 13:22:34 +0300

    ArrayList VS Linked List

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,25 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new LinkedList<>();
+        long start = System.nanoTime();
+        for (int i = 0; i < 10000; i++) {
+            list1.add(i);
+        }
+        long end = System.nanoTime();
+        System.out.println(end-start);
 
-        System.out.println("Hello world!");
+        start = System.nanoTime();
+        for (int i = 0; i < 10000; i++) {
+            list2.add(i);
+        }
+        end = System.nanoTime();
+        System.out.println(end-start);
     }
 }
