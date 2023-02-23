e61a5c39673636c98b78bbaa290da9fbf6b17840
MikleBuntin <BuntinMA@mail.ru>
2023-02-23 16:14:50 +0300

    Task 3. Решено при помощи Stack.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,55 +3,55 @@ package org.example;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
-//        List<Integer> list1 = new ArrayList<>();
-//        List<Integer> list2 = new LinkedList<>();
-//        long start = System.nanoTime();
-//        for (int i = 0; i < 10000; i++) {
-//            list1.add(i);
-//        }
-//        long end = System.nanoTime();
-//        System.out.println(end-start);
+
+//        List<String> names = new LinkedList<>();
+//        Scanner iScanner = new Scanner(System.in);
+//        String name = null;
+//        Integer index = 0;
+//        names.add(0, null);
+//        while(true) {
+//            String str = iScanner.nextLine();
+//            if (str.equals("Q")) //Выход из цикла
+//                break;
+//            String[] strArray = str.split("~"); //str[0] - слово до ~, str[1] - номер
+//            name = strArray[0];
+//            index = Integer.parseInt(strArray[1]);
 //
-//        start = System.nanoTime();
-//        for (int i = 0; i < 10000; i++) {
-//            list2.add(i);
+//            if (name.equals("print")) {
+//                System.out.println(names.get(index));
+//                names.remove(index);
+//            }
+//            else if (index > names.size()) {
+//                for (int i = names.size(); i < index; i++){
+//                    names.add(i, null);
+//                }
+//                names.add(index, name);
+//            }
+//            else names.add(index, name);
 //        }
-//        end = System.nanoTime();
-//        System.out.println(end-start);
-        List<String> names = new LinkedList<>();
+//iScanner.close();
+
+        Stack<String> stack = new Stack<>();
+
         Scanner iScanner = new Scanner(System.in);
-        String name = null;
-        Integer index = 0;
-        names.add(0, null);
+//        String name = null;
+//        Integer index = 0;
         while(true) {
             String str = iScanner.nextLine();
+
             if (str.equals("Q")) //Выход из цикла
                 break;
-            String[] strArray = str.split("~"); //str[0] - слово до ~, str[1] - номер
-            name = strArray[0];
-            index = Integer.parseInt(strArray[1]);
 
-            if (name.equals("print")) {
-                System.out.println(names.get(index));
-                names.remove(index);
+            if (str.equals("print")) {
+                System.out.println(stack.pop());
+//                names.remove(index);
             }
-            else if (index > names.size()) {
-                for (int i = names.size(); i < index; i++){
-                    names.add(i, null);
-                }
-                names.add(index, name);
-
+            else stack.push(str);
             }
-            else names.add(index, name);
-
-
-
-        }
-iScanner.close();
-
     }
 }
